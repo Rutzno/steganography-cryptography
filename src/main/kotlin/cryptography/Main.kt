347c9ff46e6b5c1c73835cb832ece702d9b17531
Rutzno <d.rutzno@gmail.com>
2021-11-21 16:42:37 +0100

    Stage 3

--- src/main/kotlin/cryptography/Main.kt
+++ src/main/kotlin/cryptography/Main.kt
@@ -62,6 +62,7 @@ fun show() {
     secretMessageBA.removeAt(--index)
     secretMessageBA.removeAt(--index)
     secretMessageBA.removeAt(--index)
+
     val message = secretMessageBA.toByteArray().toString(Charsets.UTF_8)
     println("Message:\n$message")
 }
